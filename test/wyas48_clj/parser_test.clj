e0a746e4acd296ec75fc11bbb7e5e1d82ae500fa
Colin Drake <colin.f.drake@gmail.com>
2016-02-18 18:56:54 -0500

    Update parser tests

--- test/wyas48_clj/parser_test.clj
+++ test/wyas48_clj/parser_test.clj
@@ -2,34 +2,50 @@
   (:require [clojure.test :refer :all]
             [wyas48-clj.parser :refer :all]))
 
+(defn- test-parse
+  "Test helper for parsing tests."
+  [input expected]
+  (let [[status [result]] (parse-string input)]
+    (is (and (= status :success) (= result expected)))))
+
 (deftest number-parsing-test
   (testing "It can parse numbers"
-    (is (= (parse-string "34") [:number 34]))
-    (is (= (parse-string "10000") [:number 10000]))
-    (is (= (parse-string "0") [:number 0]))))
+    (test-parse "34" [:number 34])
+    (test-parse "10000" [:number 10000])
+    (test-parse "0" [:number 0])))
 
 (deftest string-parsing-test
   (testing "It can parse strings"
-    (is (= (parse-string "\"hello\"") [:string "hello"]))
-    (is (= (parse-string "\"\"") [:string ""]))))
+    (test-parse "\"hello\"" [:string "hello"])
+    (test-parse "\"\"" [:string ""])))
+
+(deftest bool-parsing-test
+  (testing "It can parse booleans"
+    (test-parse "#t" [:bool true])
+    (test-parse "#f" [:bool false])))
 
 (deftest atom-parsing-test
   (testing "It can parse atoms"
-    (is (= (parse-string "test") [:atom "test"]))
-    (is (= (parse-string "test-atom") [:atom "test-atom"]))
-    (is (= (parse-string "*test-atom*") [:atom "*test-atom*"]))
-    (is (= (parse-string "!!#$") [:atom "!!#$"]))
-    (is (= (parse-string "+") [:atom "+"]))))
+    (test-parse "test" [:atom "test"])
+    (test-parse "test-atom" [:atom "test-atom"])
+    (test-parse "*test-atom*" [:atom "*test-atom*"])
+    (test-parse "!!#$" [:atom "!!#$"])
+    (test-parse "+" [:atom "+"])))
 
 (deftest list-parsing-test
   (testing "It can parse lists"
-    (is (= (parse-string "()") [:list]))
-    (is (= (parse-string "(23 24)") [:list [:number 23] [:number 24]]))
-    (is (= (parse-string "(23 24 test)") [:list [:number 23] [:number 24] [:atom "test"]]))
-    (is (= (parse-string "(23 24 (test 34))") [:list [:number 23] [:number 24] [:list [:atom "test"] [:number 34]]]))))
+    (test-parse "()" [:list])
+    (test-parse "(23 24)" [:list [:number 23] [:number 24]])
+    (test-parse "(23 24 test)" [:list [:number 23] [:number 24] [:atom "test"]])
+    (test-parse "(23 24 (test 34))" [:list [:number 23] [:number 24] [:list [:atom "test"] [:number 34]]])))
 
 (deftest quote-parsing-test
-  (testing "It can parse syntax quotes"
-    (is (= (parse-string "'34") [:list [:atom "quote"] [:number 34]]))
-    (is (= (parse-string "'()") [:list [:atom "quote"] [:list]]))
-    (is (= (parse-string "'(3 4 5)") [:list [:atom "quote"] [:list [:number 3] [:number 4] [:number 5]]]))))
+  (testing "It can parse quotes"
+    (test-parse "'34" [:list [:atom "quote"] [:number 34]])
+    (test-parse "'()" [:list [:atom "quote"] [:list]])
+    (test-parse "'(3 4 5)" [:list [:atom "quote"] [:list [:number 3] [:number 4] [:number 5]]])))
+
+(deftest dotted-parsing-test
+  (testing "It can parse dotted pairs"
+    (test-parse "(3 . 4)" [:dotted [:number 3] [:number 4]])
+    (test-parse "(3 . (4 . ()))" [:dotted [:number 3] [:dotted [:number 4] [:list]]])))
