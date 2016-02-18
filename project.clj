a2a89be9dda73427fa0321705934da5b0b73703a
Colin Drake <colin.f.drake@gmail.com>
2016-02-18 17:48:03 -0500

    Add in JLine dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.4.1"]]
+                 [instaparse "1.4.1"]
+                 [jline "0.9.94"]]
   :main ^:skip-aot wyas48-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
