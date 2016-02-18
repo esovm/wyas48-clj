694743c5b6e86da7cf370d3c6fe65361330c7f66
Colin Drake <colin.f.drake@gmail.com>
2016-02-18 17:49:26 -0500

    Return an array of parsed results

--- src/wyas48_clj/parser.clj
+++ src/wyas48_clj/parser.clj
@@ -7,15 +7,18 @@
 
 (def ^:private transformations
   "Transformations to apply to parsed nodes."
-  {:atom   (fn [a] [:atom (str a)])
+  {:bool   (fn [b] [:bool (= "#t" b)])
+   :atom   (fn [a] [:atom (str a)])
    :number (fn [n] [:number (Integer/parseInt n)])
    :quoted (fn [q] [:list [:atom "quote"] q])
    :expr   (fn [e] e)})
 
 (defn parse-string
-  "Parses a string into an expression representation."
+  "Parses a string into an expression representation.
+  Returns a sequence of parsed expressions."
   [input-string]
-  (->> input-string
-       parser
-       (insta/transform transformations)
-       first))
+  (let [transformer (partial insta/transform transformations)
+        result (-> input-string parser)]
+    (if (insta/failure? result)
+      [:failure result]
+      [:success (map transformer result)])))
