60b1b77cb3bb6b7d2aee877d923d35b798031b7c
Colin Drake <colin.f.drake@gmail.com>
2016-02-18 19:41:05 -0500

    Fix copy-pasted test typos

--- test/wyas48_clj/printer_test.clj
+++ test/wyas48_clj/printer_test.clj
@@ -13,32 +13,32 @@
     (test-print [:number 10000] "10000")
     (test-print [:number 0] "0")))
 
-(deftest string-parsing-test
-  (testing "It can parse strings"
+(deftest string-printing-test
+  (testing "It can print strings"
     (test-print [:string "hello"] "\"hello\"")
     (test-print [:string ""] "\"\"")))
 
-(deftest bool-parsing-test
-  (testing "It can parse booleans"
+(deftest bool-printing-test
+  (testing "It can print booleans"
     (test-print [:bool true] "#t")
     (test-print [:bool false] "#f")))
 
-(deftest atom-parsing-test
-  (testing "It can parse atoms"
+(deftest atom-printing-test
+  (testing "It can print atoms"
     (test-print [:atom "test"] "test")
     (test-print [:atom "test-atom"] "test-atom")
     (test-print [:atom "*test-atom*"] "*test-atom*")
     (test-print [:atom "!!#$"] "!!#$")
     (test-print [:atom "+"] "+")))
 
-(deftest list-parsing-test
-  (testing "It can parse lists"
+(deftest list-printing-test
+  (testing "It can print lists"
     (test-print [:list] "()")
     (test-print [:list [:number 23] [:number 24]] "(23 24)")
     (test-print [:list [:number 23] [:number 24] [:atom "test"]] "(23 24 test)")
     (test-print [:list [:number 23] [:number 24] [:list [:atom "test"] [:number 34]]] "(23 24 (test 34))")))
 
-(deftest dotted-parsing-test
-  (testing "It can parse dotted pairs"
+(deftest dotted-printing-test
+  (testing "It can print dotted pairs"
     (test-print [:dotted [:number 3] [:number 4]] "(3 . 4)")
     (test-print [:dotted [:number 3] [:dotted [:number 4] [:list]]] "(3 . (4 . ()))")))
