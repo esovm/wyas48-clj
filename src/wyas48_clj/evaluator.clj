6346a3067bce0eaa6e0da3f2dd7f4a4b7ebf6b4b
Colin Drake <colin.f.drake@gmail.com>
2016-02-19 13:27:33 -0500

    Add in type coercer functions and typed-binary-primitive for =, >, <, and /=

--- src/wyas48_clj/evaluator.clj
+++ src/wyas48_clj/evaluator.clj
@@ -2,6 +2,8 @@
   (:require [clojure.core.match :refer [match]]
             [wyas48-clj.exceptions :refer :all]))
 
+;;; Type coercion functions
+
 (defn- coerce-to-number
   "Attempts to coerce an expression into a number."
   [e]
@@ -11,6 +13,34 @@
                           (catch Exception ex (throw (type-mismatch-exception "number" e))))
     [:list [n]]      (coerce-to-number n)
     :else            (throw (type-mismatch-exception "number" e))))
+
+(defn- coerce-to-string
+  "Attempts to coerce an expression into a string."
+  [e]
+  (match e
+    [:string string] string
+    [:number num]    (str num)
+    [:bool b]        (str b)
+    :else            (throw (type-mismatch-exception "string" e))))
+
+(defn- coerce-to-bool
+  "Attempts to coerce an expression into a bool."
+  [e]
+  (match e
+    [:bool b] b
+    :else     (throw (type-mismatch-exception "bool" e))))
+
+;;; Primitive HOFs
+;;; Functions that *produce* primitive functions to be loaded in the environment
+
+(defn- typed-binary-primitive
+  "Returns a primitive function performing a *strictly* binary version of f among its arguments."
+  [f result-type coercer]
+  (fn [& args]
+    (if (= 2 (count args))
+      (let [[arg1 arg2] (map coercer args)]
+        [result-type (f arg1 arg2)])
+      (throw (invalid-argument-count-exception 2 args)))))
 
 (defn- numeric-binary-primitive
   "Returns a primitive function performing a folded version of f among its arguments."
@@ -24,15 +54,18 @@
   "Returns a primitive function that tests an argument against the given type."
   [type]
   (fn [arg]
-    [:bool (= (first arg) type)]))
+    (match arg
+      [arg-type _] [:bool (= type arg-type)])))
 
-(defn- swap-values-primitive
+(defn- swap-types-primitive
   "Returns a primitive function that swaps values for from and to types."
   [from from-type-display-name to]
   (fn [arg]
     (match arg
       [from val] [to val]
       :else (throw (type-mismatch-exception from-type-display-name arg)))))
+
+;;; Built-in primitives & evaluation
 
 (def ^:private primitives
   "Primitive, built-in operations."
@@ -46,8 +79,12 @@
    "symbol?" (type-testing-primitive :atom)
    "string?" (type-testing-primitive :string)
    "number?" (type-testing-primitive :number)
-   "symbol->string" (swap-values-primitive :atom "symbol" :string)
-   "string->symbol" (swap-values-primitive :string "string" :atom)})
+   "symbol->string" (swap-types-primitive :atom "symbol" :string)
+   "string->symbol" (swap-types-primitive :string "string" :atom)
+   "=" (typed-binary-primitive = :bool coerce-to-number)
+   ">" (typed-binary-primitive > :bool coerce-to-number)
+   "<" (typed-binary-primitive < :bool coerce-to-number)
+   "/=" (typed-binary-primitive (complement =) :bool coerce-to-number)})
 
 (defn- apply-func
   "Function application."
