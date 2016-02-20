da66c332aef8528888fe431798318a635500d3cd
Colin Drake <colin.f.drake@gmail.com>
2016-02-20 12:05:53 -0500

    Refactor REPL logic into separate namespace

--- src/wyas48_clj/core.clj
+++ src/wyas48_clj/core.clj
@@ -1,69 +1,7 @@
 (ns wyas48-clj.core
-  (:require [clojure.string :refer [trim]]
-            [clojure.core.match :refer [match]]
-            [wyas48-clj.evaluator :refer [evaluate primitive-names]]
-            [wyas48-clj.parser :refer [parse-string]]
-            [wyas48-clj.printer :refer [expr->string]])
-  (:import (jline ConsoleReader SimpleCompletor))
+  (:require [clojure.core.match :refer [match]]
+            [wyas48-clj.repl :refer [run-one repl]])
   (:gen-class))
-
-(defn- balanced?
-  "Predicate determining if the input string, s, has balanced parenthesis.
-  If no parenthesis are in the string, true is returned."
-  [s]
-  (loop [check-str (seq s), open 0]
-    (if (seq? check-str)
-      (case (first check-str)
-        \( (recur (next check-str) (inc open))
-        \) (recur (next check-str) (dec open))
-        (recur (next check-str) open))
-      (zero? open))))
-
-(defn- read-until-balanced
-  "Returns a string, enforcing balanced parenthesis.
-  Continues prompting until the input is well-formed."
-  [reader]
-  (loop [buffer ""]
-    (let [line (-> (.readLine reader) trim)
-          total-input (str buffer "\n" line)]
-      (cond
-        (= "\n" total-input)    ""
-        (balanced? total-input) total-input
-        :else                   (do (print "    ... > ")
-                                    (flush)
-                                    (recur total-input))))))
-
-(defn- rep
-  "Read-Eval-Print... A single rep of a REPL? ;)"
-  [input]
-  (try
-    (let [result (parse-string input)]
-      (doseq [expr result]
-        (-> expr evaluate expr->string println)))
-    (catch Exception e (println (.getMessage e)))))
-
-(defn- repl
-  "Implementation of the main Read-Eval-Print-Loop."
-  []
-  (let [reader (ConsoleReader.)
-        primitive-prefixes (map #(str "(" %) primitive-names)
-        primitive-completor (SimpleCompletor. (into-array primitive-prefixes))]
-    (.addCompletor reader primitive-completor)
-    (while true
-      (print "Scheme>>> ")
-      (flush)
-      (let [input (read-until-balanced reader)]
-        (cond
-          ;; Exit condition.
-          (or (= input "quit") (= input "exit"))
-            (do (println "Exiting...")
-                (System/exit 0))
-          ;; Empty line.
-          (= "" input)
-            (do (println) (flush))
-          ;; Valid input.
-          :else
-            (rep input))))))
 
 (defn- die
   "Exits the program and returns a status to the OS."
@@ -76,5 +14,5 @@
   [& args]
   (match (vec args)
     []      (repl)
-    [input] (rep input)
+    [input] (run-one input)
     :else   (die "Invalid command line arguments provided." false)))
---
+++ src/wyas48_clj/repl.clj
@@ -0,0 +1,70 @@
+(ns wyas48-clj.repl
+  (:require [clojure.string :refer [trim]]
+            [clojure.core.match :refer [match]]
+            [wyas48-clj.evaluator :refer [evaluate primitive-names]]
+            [wyas48-clj.parser :refer [parse-string]]
+            [wyas48-clj.printer :refer [expr->string]])
+  (:import (jline ConsoleReader SimpleCompletor)))
+
+(defn- balanced?
+  "Predicate determining if the input string, s, has balanced parenthesis.
+  If no parenthesis are in the string, true is returned."
+  [s]
+  (loop [check-str (seq s), open 0]
+    (if (seq? check-str)
+      (case (first check-str)
+        \( (recur (next check-str) (inc open))
+        \) (recur (next check-str) (dec open))
+        (recur (next check-str) open))
+      (zero? open))))
+
+(defn- read-until-balanced
+  "Returns a string, enforcing balanced parenthesis.
+  Continues prompting until the input is well-formed."
+  [reader]
+  (loop [buffer ""]
+    (let [line (-> (.readLine reader) trim)
+          total-input (str buffer "\n" line)]
+      (cond
+        (= "\n" total-input)    ""
+        (balanced? total-input) total-input
+        :else                   (do (print "    ... > ")
+                                    (flush)
+                                    (recur total-input))))))
+
+(defn- create-completor
+  "Creates a *simple* completor based off of the given function names."
+  [function-names]
+  (let [names-array (into-array (map #(str "(" %) function-names))]
+    (SimpleCompletor. names-array)))  ;; TODO: Make this smarter/better.
+
+(defn run-one
+  "Read-Eval-Print... A single rep of a REPL? ;)"
+  [input]
+  (try
+    (let [result (parse-string input)]
+      (doseq [expr result]
+        (-> expr evaluate expr->string println)))
+    (catch Exception e (println (.getMessage e)))))
+
+(defn repl
+  "Implementation of the main Read-Eval-Print-Loop."
+  []
+  (let [reader (ConsoleReader.)
+        primitive-completor (create-completor primitive-names)]
+    (.addCompletor reader primitive-completor)
+    (while true
+      (print "Scheme>>> ")
+      (flush)
+      (let [input (read-until-balanced reader)]
+        (cond
+          ;; Exit condition.
+          (or (= input "quit") (= input "exit"))
+            (do (println "Exiting...")
+                (System/exit 0))
+          ;; Empty line.
+          (= "" input)
+            (do (println) (flush))
+          ;; Valid input.
+          :else
+            (run-one input))))))
