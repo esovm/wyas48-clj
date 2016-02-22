85caf7b5c62bc561a09213e37220c44d36103723
Colin Drake <colin.f.drake@gmail.com>
2016-02-21 20:21:44 -0500

    Add in environment tests

--- test/wyas48_clj/evaluator_test.clj
+++ test/wyas48_clj/evaluator_test.clj
@@ -42,7 +42,7 @@
     (is (= (evaluate [:list [:atom "symbol->string"] [:list [:atom "quote"] [:atom "test"]]] test-env) [:string "test"]))
     (is (= (evaluate [:list [:atom "string->symbol"] [:string "some-symbol"]] test-env) [:atom "some-symbol"]))))
 
-(deftest evaluation-part-2-test
+(deftest evaluation-part-2-comparisons-test
   (testing
     (is (= (evaluate [:list [:atom "="] [:number 3] [:number 5]] test-env) [:bool false]))
     (is (= (evaluate [:list [:atom "="] [:number 3] [:number 3]] test-env) [:bool true]))
@@ -54,3 +54,9 @@
     (is (= (evaluate [:list [:atom ">="] [:number 5] [:number 15]] test-env) [:bool false]))
     (is (= (evaluate [:list [:atom "<="] [:number 15] [:number 5]] test-env) [:bool false]))
     (is (= (evaluate [:list [:atom "<="] [:number 5] [:number 15]] test-env) [:bool true]))))
+
+(def populated-test-env (atom {"a" [:number 3]}))
+
+(deftest evaluation-variables-assignments-test
+  (testing
+    (is (= (evaluate [:atom "a"] populated-test-env) [:number 3]))))
