7eb100a9a539705efca5ce4ae4ccf6398fb0998d
Colin Drake <colin.f.drake@gmail.com>
2016-02-21 20:08:02 -0500

    Update tests to use environments

--- test/wyas48_clj/evaluator_test.clj
+++ test/wyas48_clj/evaluator_test.clj
@@ -2,40 +2,42 @@
   (:require [clojure.test :refer :all]
             [wyas48-clj.evaluator :refer :all]))
 
+(def test-env (atom (make-environment)))
+
 (deftest string-evaluation-test
   (testing
-    (is (= (evaluate [:string "test"]) [:string "test"]))))
+    (is (= (evaluate [:string "test"] test-env) [:string "test"]))))
 
 (deftest bool-evaluation-test
   (testing
-    (is (= (evaluate [:bool true]) [:bool true]))
-    (is (= (evaluate [:bool false]) [:bool false]))))
+    (is (= (evaluate [:bool true] test-env) [:bool true]))
+    (is (= (evaluate [:bool false] test-env) [:bool false]))))
 
 (deftest number-evaluation-test
   (testing
-    (is (= (evaluate [:number 42]) [:number 42]))))
+    (is (= (evaluate [:number 42] test-env) [:number 42]))))
 
 (deftest number-evaluation-test
   (testing
-    (is (= (evaluate [:list [:atom "quote"] [:number 42]]) [:number 42]))))
+    (is (= (evaluate [:list [:atom "quote"] [:number 42]] test-env) [:number 42]))))
 
 (deftest evaluation-part-1-test
   (testing
-    (is (= (evaluate [:list [:atom "+"] [:number 3] [:number 5]]) [:number 8]))
-    (is (= (evaluate [:list [:atom "-"] [:number 3] [:number 5]]) [:number -2]))
-    (is (= (evaluate [:list [:atom "*"] [:number 3] [:number 5]]) [:number 15]))
-    (is (= (evaluate [:list [:atom "/"] [:number 15] [:number 5]]) [:number 3]))
-    (is (= (evaluate [:list [:atom "mod"] [:number 15] [:number 5]]) [:number 0]))
-    (is (= (evaluate [:list [:atom "quotient"] [:number 15] [:number 2]]) [:number 7]))
-    (is (= (evaluate [:list [:atom "remainder"] [:number 15] [:number 2]]) [:number 1]))))
+    (is (= (evaluate [:list [:atom "+"] [:number 3] [:number 5]] test-env) [:number 8]))
+    (is (= (evaluate [:list [:atom "-"] [:number 3] [:number 5]] test-env) [:number -2]))
+    (is (= (evaluate [:list [:atom "*"] [:number 3] [:number 5]] test-env) [:number 15]))
+    (is (= (evaluate [:list [:atom "/"] [:number 15] [:number 5]] test-env) [:number 3]))
+    (is (= (evaluate [:list [:atom "mod"] [:number 15] [:number 5]] test-env) [:number 0]))
+    (is (= (evaluate [:list [:atom "quotient"] [:number 15] [:number 2]] test-env) [:number 7]))
+    (is (= (evaluate [:list [:atom "remainder"] [:number 15] [:number 2]] test-env) [:number 1]))))
 
 (deftest evaluation-part-1-additions-test
   (testing
-    (is (= (evaluate [:list [:atom "string?"] [:string "hello"]]) [:bool true]))
-    (is (= (evaluate [:list [:atom "string?"] [:number 15]]) [:bool false]))
-    (is (= (evaluate [:list [:atom "number?"] [:number 15]]) [:bool true]))
-    (is (= (evaluate [:list [:atom "number?"] [:string "15"]]) [:bool false]))
-    (is (= (evaluate [:list [:atom "symbol?"] [:list [:atom "quote"] [:atom "test"]]]) [:bool true]))
-    (is (= (evaluate [:list [:atom "symbol?"] [:string "bad"]]) [:bool false]))
-    (is (= (evaluate [:list [:atom "symbol->string"] [:list [:atom "quote"] [:atom "test"]]]) [:string "test"]))
-    (is (= (evaluate [:list [:atom "string->symbol"] [:string "some-symbol"]]) [:atom "some-symbol"]))))
+    (is (= (evaluate [:list [:atom "string?"] [:string "hello"]] test-env) [:bool true]))
+    (is (= (evaluate [:list [:atom "string?"] [:number 15]] test-env) [:bool false]))
+    (is (= (evaluate [:list [:atom "number?"] [:number 15]] test-env) [:bool true]))
+    (is (= (evaluate [:list [:atom "number?"] [:string "15"]] test-env) [:bool false]))
+    (is (= (evaluate [:list [:atom "symbol?"] [:list [:atom "quote"] [:atom "test"]]] test-env) [:bool true]))
+    (is (= (evaluate [:list [:atom "symbol?"] [:string "bad"]] test-env) [:bool false]))
+    (is (= (evaluate [:list [:atom "symbol->string"] [:list [:atom "quote"] [:atom "test"]]] test-env) [:string "test"]))
+    (is (= (evaluate [:list [:atom "string->symbol"] [:string "some-symbol"]] test-env) [:atom "some-symbol"]))))
